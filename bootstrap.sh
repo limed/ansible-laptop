7633212a574d534a39d343c2ca0fbf3600402e2a
limed <limed@mozilla.com>
2016-04-15 14:04:01 -0700

    Fixing up bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# exit on command fail and error on unset variable
+set -eu
+
 echo "Installing homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Create ~/git directory"
+mkdir ~/git
+
+echo "Cloning ansible playbook into ~/git directory"
+git clone git@github.com:limed/ansible-laptop.git ~/git/ansible-laptop
