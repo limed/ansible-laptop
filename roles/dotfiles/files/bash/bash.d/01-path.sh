7a7440285d620cbe13b51cfa7e43cfb43668e023
limed <limed@mozilla.com>
2016-09-29 09:21:00 -0700

    Syncing bash files

--- roles/dotfiles/files/bash/bash.d/01-path.sh
+++ roles/dotfiles/files/bash/bash.d/01-path.sh
@@ -1,16 +1,19 @@
 # Path environment variable goes here
 
 NUBIS_BUILDER_PATH="${HOME}/nubis/nubis-builder/bin"
+GOPATH="${HOME}/go"
+
 # basic os detection
 case "$OSTYPE" in
     darwin*)
-        PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:${HOME}/bin:${NUBIS_BUILDER_PATH}:$PATH"
+        PATH="$(/usr/local/bin/brew --prefix coreutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:${HOME}/bin:${NUBIS_BUILDER_PATH}:${GOPATH}/bin:$PATH"
     ;;
     linux*)
-        PATH="/usr/local/bin:/usr/local/sbin:${HOME}/bin:${NUBIS_BUILDER_PATH}:$PATH"
+        PATH="/usr/local/bin:/usr/local/sbin:${HOME}/bin:${NUBIS_BUILDER_PATH}:${GOPATH}/bin:$PATH"
     ;;
     *)
-        PATH="${HOME}/bin:$PATH"
+        PATH="/usr/local/bin:/usr/local/sbin:${HOME}/bin:${GOPATH}/bin:$PATH"
 esac
 export PATH
-
+export GOPATH
+export GO15VENDOREXPERIMENT=1
--- roles/dotfiles/files/bash/bash.d/functions.sh
+++ roles/dotfiles/files/bash/bash.d/functions.sh
@@ -14,23 +14,19 @@ function get_ldap_password_gpg() {
 }
 
 function ldap_query_admin() {
-    ldapsearch -x -D "mail=elim_la@mozilla.com,o=com,dc=mozilla" -w "$(get_ldap_password mozilla_la.asc)" -h ldap.db.scl3.mozilla.com -b dc=mozilla $1 $2
+    ldapsearch -LLL -o ldif-wrap=no -x -D "mail=elim_la@mozilla.com,o=com,dc=mozilla" -w "$(get_ldap_password mozilla_la.asc)" -h 127.0.0.1 -p 9090 -b dc=mozilla $1 $2
 }
 
 function ldap_query() {
-    ldapsearch -x -D "mail=elim@mozilla.com,o=com,dc=mozilla" -w "$(get_ldap_password_gpg)" -h ldap.db.scl3.mozilla.com -b dc=mozilla $1 $2
+    ldapsearch -LLL -o ldif-wrap=no -x -D "mail=elim@mozilla.com,o=com,dc=mozilla" -w "$(get_ldap_password_gpg)" -h 127.0.0.1 -p 9090 -b dc=mozilla $1 $2
 }
 
 function load_ldif() {
     local ldif_file=$1
-
-    local bind_user="mail=elim_la@mozilla.com,o=com,dc=mozilla"
-    local ldap_master="ldapmaster1.db.scl3.mozilla.com"
-
-    read -p "You are about to load ${ldif_file} to ${ldap_master}, are you sure? [y/n]: " answer
+    read -p "You are about to load ${ldif_file} to ldapmaster1.db.scl3.mozilla.com, are you sure? [y/n]: " answer
     if [[ $answer == 'y' ]]; then
         echo "Loading ldif file: ${ldif_file}"
-        ldapmodify -x -D "mail=elim@mozilla.com,o=com,dc=mozilla" -w "$(get_ldap_password_gpg)" -h ${ldap_master} -f $1
+        ldapmodify -x -D "mail=elim_la@mozilla.com,o=com,dc=mozilla" -w "$(get_ldap_password mozilla_la.asc)" -h ldapmaster1.db.scl3.mozilla.com -f $1
         return 0
     elif [[ $answer == 'n' ]]; then
         echo "Not doing anything, exiting"
@@ -43,10 +39,10 @@ function load_ldif() {
 
 function load_ldif_dev() {
     local ldif_file=$1
-    read -p "You are about to load ${ldif_file} to ldapmaster1.dev.db.phx1.mozilla.com, are you sure? [y/n]: " answer
+    read -p "You are about to load ${ldif_file} to ldapmaster1.dev.db.scl3.mozilla.com, are you sure? [y/n]: " answer
     if [[ $answer == 'y' ]]; then
         echo "Loading ldif file: ${ldif_file}"
-        ldapmodify -x -D "mail=elim@mozilla.com,o=com,dc=mozilla" -w "$(get_ldap_password_gpg)" -h ldapmaster1.dev.db.scl3.mozilla.com -f $1
+        ldapmodify -x -D "mail=elim_la@mozilla.com,o=com,dc=mozilla" -w "$(get_ldap_password mozilla_la.asc)" -h ldapmaster1.dev.db.scl3.mozilla.com -f $1
         return 0
     elif [[ $answer == 'n' ]]; then
         echo "Not doing anything, exiting"
@@ -55,4 +51,44 @@ function load_ldif_dev() {
         echo "Invalid input"
         return 1
     fi
+}
+
+function myip() {
+    local arg=$1
+
+    local curl_cmd='curl --retry 5 -s -fq http://ipinfo.io'
+    local output
+    if [[ "$arg" == '--verbose' ]]; then
+        output=$(${curl_cmd} | python -m'json.tool')
+    else
+        output=$(${curl_cmd} | python -c 'import sys, json; print json.load(sys.stdin)["ip"]')
+    fi
+    echo "${output}"
+}
+
+function gocd() {
+    cd $(go list -f '{{.Dir}}' $1)
+}
+
+function aws-vault() {
+    local aws_vault_cmd=$(which aws-vault)
+
+    if [ -z "${aws_vault_cmd}" ]; then
+        echo "aws-vault is not on your path you do not have it installed"
+        exit 1
+    fi
+
+    case "$1" in
+        exec)
+            local action=$1
+            shift 1
+            local profile=$1
+            shift 1
+            local options="--assume-role-ttl=1h --session-ttl=4h"
+            ${aws_vault_cmd} ${action} ${profile} ${options} -- $@
+        ;;
+        *)
+            ${aws_vault_cmd} $@
+        ;;
+    esac
 }
