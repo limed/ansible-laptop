f23c842c5ccd2454557491d0ddd2ddc96cfb63ca
limed <limed@mozilla.com>
2018-08-23 15:20:09 -0700

    Updating bash dotfile

--- roles/dotfiles/files/bash/bash.d/01-path.sh
+++ roles/dotfiles/files/bash/bash.d/01-path.sh
@@ -2,13 +2,23 @@
 
 GOPATH="${HOME}/go"
 
+brew_get_prefix() {
+    local package=$1
+    if [ "$(uname -s)" == "Darwin" ]; then
+        /usr/local/bin/brew --prefix "${package}"
+    fi
+}
+
 # basic os detection
 case "$OSTYPE" in
     darwin*)
-        PATH="$(/usr/local/bin/brew --prefix coreutils)/libexec/gnubin:$(/usr/local/bin/brew --prefix findutils)/libexec/gnubin:/usr/local/bin:/usr/local/sbin:${HOME}/bin:${GOPATH}/bin:$PATH"
+        COREUTIL_PATH="$(brew_get_prefix coreutils)/libexec/gnubin"
+        GNUSED_PATH="$(brew_get_prefix gnu-sed)/libexec/gnubin"
+        FINDUTIL_PATH="$(brew_get_prefix findutils)/libexec/gnubin"
+        PATH="${GNUSED_PATH}:${COREUTIL_PATH}:${FINDUTIL_PATH}:/usr/local/bin:/usr/local/sbin:${HOME}/bin:${GOPATH}/bin:$PATH"
     ;;
     linux*)
-        PATH="/usr/local/bin:/usr/local/sbin:${HOME}/bin:${NUBIS_BUILDER_PATH}:${GOPATH}/bin:$PATH"
+        PATH="/usr/local/bin:/usr/local/sbin:${HOME}/bin:${GOPATH}/bin:$PATH"
     ;;
     *)
         PATH="/usr/local/bin:/usr/local/sbin:${HOME}/bin:${GOPATH}/bin:$PATH"
