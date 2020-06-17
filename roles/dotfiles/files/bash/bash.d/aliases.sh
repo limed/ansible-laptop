cd47eda742d0122690e158144a7b8f823cc3d00c
limed <limed@mozilla.com>
2020-06-17 14:52:47 -0700

    Add aliases

--- roles/dotfiles/files/bash/bash.d/aliases.sh
+++ roles/dotfiles/files/bash/bash.d/aliases.sh
@@ -1,6 +1,6 @@
 # enable color support of ls and also add handy aliases
 if [ "$TERM" != "dumb" ]; then
-    eval "`dircolors -b`"
+    eval "$(dircolors -b)"
     alias ls='ls --color=auto'
 fi
 
@@ -24,9 +24,12 @@ alias ncmpc='ncmpc --colors'
 alias gam='~/gam/gam'
 alias git='hub'
 alias awsenv='env | grep AWS'
+alias awsexpire="date -d @${AWS_SESSION_EXPIRATION}"
 alias kubeenv='env | grep KUBECONFIG'
 alias nubisctl='~/nubis/nubis-ctl/nubis-ctl'
 alias saferm='shred -vfz -u -n 10'
 alias terrafmt="terraform fmt -write=true -diff=true"
 alias kns="kubens"
 alias kctx="kubectx"
+alias k="kubectl"
+alias awsunset='unset AWS_ACCESS_KEY_ID; unset AWS_SECRET_ACCESS_KEY; unset AWS_SESSION_TOKEN; unset AWS_MAWS_PROFILE; unset AWS_SESSION_EXPIRATION;'
